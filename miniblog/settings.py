7fbdbeb99ebfcfdc117332d61159f7ad03dafd44
Ali Sher - Software Engineer and Developer <alisher.201903953@gcuf.edu.pk>
2024-01-11 10:19:39 +0500

    Update settings.py

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -73,16 +73,19 @@ WSGI_APPLICATION = 'miniblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myappdb',
-        'USER': 'myappuser',
-        'PASSWORD': 'as137669',
-        'HOST': 'myusername-667.postgres.pythonanywhere-services.com',
-        'PORT': 10667,
+        'NAME': os.getenv('mydb'),
+        'USER': os.getenv('ali'),
+        'PASSWORD': os.getenv('as137669'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 
 # Password validation
