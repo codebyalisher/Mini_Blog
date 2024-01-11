9b6e08d02a35afe45254d2d0bc0fbf7c892376e4
Ali Sher - Software Engineer and Developer <alisher.201903953@gcuf.edu.pk>
2024-01-11 08:34:00 +0500

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,9 @@
 
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r django
-python3.9 -m pip install -r requirements.txt
+pip uninstall -r requirements.txt
+pip install -r requirements.txt
+
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
